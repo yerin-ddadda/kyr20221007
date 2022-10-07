1a3fcbd2a655f1a0a2ff52822783cf8cdc1cb775
yerin-ddadda <yerin0110@naver.com>
2022-10-07 15:07:24 +0900

    메인 ui 2 작업 및 회원 name 추가

--- webstudy99-kyr221007/src/main/java/org/kosta/projectname/model/MemberVO.java
+++ webstudy99-kyr221007/src/main/java/org/kosta/projectname/model/MemberVO.java
@@ -2,13 +2,22 @@ package org.kosta.projectname.model;
 
 public class MemberVO {
 	private String id;
-
+	private String name;
+	
 	public String getId() {
 		return id;
 	}
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
